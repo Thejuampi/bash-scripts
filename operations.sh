09bbd851fe1917cde47a1171180068aff14890b0
Juan Pablo Lescano <juanpablo.lescano@gmail.com>
2023-02-14 10:59:54 -0500

    Update operations.sh

--- operations.sh
+++ operations.sh
@@ -64,3 +64,41 @@ check_jar_conflicts_v2() {
     echo "No class conflicts found."
   fi
 }
+
+# optimized version -- v1 and v2 were O(n^2)
+check_jar_conflicts_v3() {
+  local directory="$1"
+  local regex_filter="$2"
+
+  declare -A class_cache=()
+
+  while IFS= read -r -d '' jar_file; do
+    class_cache["$jar_file"]=$(jar -tf "$jar_file" | grep '\.class$' | sed 's/\.class$//' | tr '/' '.' | grep -E "$regex_filter" || true)
+  done < <(find "$directory" -name "*.jar" -print0)
+
+  local conflict_found=false
+
+  for jar_file1 in "${!class_cache[@]}"; do
+    local classes1="${class_cache[$jar_file1]}"
+    for jar_file2 in "${!class_cache[@]}"; do
+      if [ "$jar_file1" != "$jar_file2" ]; then
+        (
+          local classes2="${class_cache[$jar_file2]}"
+          local common_classes=$(comm -12 <(echo "$classes1" | sort) <(echo "$classes2" | sort))
+          if [ ! -z "$common_classes" ]; then
+            conflict_found=true
+            echo "Class conflicts found between $jar_file1 and $jar_file2:"
+            echo "$common_classes"
+          fi
+        ) &
+      fi
+    done
+  done
+
+  # Wait for all subshells to finish before returning
+  wait
+
+  if ! $conflict_found; then
+    echo "No class conflicts found."
+  fi
+}
