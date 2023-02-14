24e69e4d149d4a52cdafcb14dc99cc686d1ad963
Juan Pablo Lescano <juanpablo.lescano@gmail.com>
2023-02-14 11:24:02 -0500

    Update operations.sh

--- operations.sh
+++ operations.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+check_jar_conflicts_sequencia_cached_only_jar_names() {
+  local directory="$1"
+  local regex_filter="$2"
+
+  declare -A class_cache=()
+
+  while IFS= read -r -d '' jar_file; do
+    class_cache["$jar_file"]=$(jar -tf "$jar_file" | grep '\.class$' | sed 's/\.class$//' | tr '/' '.' | grep -E "$regex_filter" || true)
+  done < <(find "$directory" -name "*.jar" -print0)
+
+  local conflict_found=false
+
+  for jar_file1 in "${!class_cache[@]}"; do
+    local classes1="${class_cache[$jar_file1]}"
+    for jar_file2 in "${!class_cache[@]}"; do
+      if [ "$jar_file1" != "$jar_file2" ]; then
+        local classes2="${class_cache[$jar_file2]}"
+        local common_classes=$(comm -12 <(echo "$classes1" | sort) <(echo "$classes2" | sort))
+        if [ ! -z "$common_classes" ]; then
+          conflict_found=true
+          echo "$jar_file1 and $jar_file2 have class conflicts."
+          break
+        fi
+      fi
+    done
+  done
+
+  if ! $conflict_found; then
+    echo "No class conflicts found."
+  fi
+}
 
 check_jar_conflicts_sequencial_cached() {
   local directory="$1"
