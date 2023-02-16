c9e58362a676b31aa823c18864557b0ae71920e2
Juan Pablo Lescano <juanpablo.lescano@gmail.com>
2023-02-16 16:52:03 -0500

    Update operations.sh

--- operations.sh
+++ operations.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 
+
 check_jar_conflicts() {
     local class_cache=()
     local jar_files=("$1"/*.jar)
     local max_conflicts=${2:-3}
 
-    # Extract the list of class files and their contents from all JAR files
+    # Extract the list of class files from all JAR files
     declare -A class_map
     while IFS= read -r -d '' class_file; do
         local jar_file=$(find "${jar_files[@]}" -type f -name "$(basename "$class_file")" | head -n 1)
@@ -18,21 +19,19 @@ check_jar_conflicts() {
             continue
         fi
 
-        class_map[$class_name]=$jar_name$'\n'$(unzip -p "$jar_file" "$class_file" | md5sum | cut -d ' ' -f 1)
+        class_map[$class_name]=$jar_name
     done < <(unzip -Z -1 "${jar_files[@]}" '*.class' | tr '\n' '\0')
 
     # Check for conflicts between class files
     for class_name in "${!class_map[@]}"; do
-        local jar_name=${class_map[$class_name]%%$'\n'*}
-        local md5=${class_map[$class_name]#*$'\n'}
+        local jar_name=${class_map[$class_name]}
         for other_class_name in "${!class_cache[@]}"; do
-            local other_jar_name=${class_cache[$other_class_name]%%$'\n'*}
-            local other_md5=${class_cache[$other_class_name]#*$'\n'}
-            if [[ $class_name != $other_class_name && $jar_name != $other_jar_name && $md5 == $other_md5 ]]; then
+            local other_jar_name=${class_cache[$other_class_name]}
+            if [[ $class_name != $other_class_name && $jar_name != $other_jar_name && $(basename "$class_name") == $(basename "$other_class_name") ]]; then
                 echo "Class $class_name in JAR $jar_name conflicts with class in JAR $other_jar_name"
             fi
         done
-        class_cache[$class_name]=$jar_name$'\n'$md5
+        class_cache[$class_name]=$jar_name
     done
 }
 
